62a35ef647cd0b86742a704072b8374d9e3e89db
roshan <rootking098@gmail.com>
2024-09-16 17:35:17 +0545

    chore: fix gitignore

---
+++ bonewa/settings/dev.py
@@ -0,0 +1,5 @@
+from .base import BASE_DIR
+
+DEBUG = True
+ALLOWED_HOSTS = []
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
---
+++ bonewa/settings/prod.py
@@ -0,0 +1,9 @@
+import os
+from bonewa.settings.base import BASE_DIR
+
+DEBUG = False
+ALLOWED_HOSTS = ["dev.bonewa.com"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+# DEFAULT_FROM_EMAIL = "hellobhai05@gmail.com"
+STATIC_URL = "static/"
